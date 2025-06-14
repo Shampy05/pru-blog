1e7b5c5ab74d11aa901f504e3adb3a4a56673585
Shampy05 <sharmapranshu317@gmail.com>
2025-06-14 12:59:38 +0100

    feat: changed astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://prublog.github.io',
-	base: 'pru-blog',
 	integrations: [mdx(), sitemap()],
 });
